5b9442b1042fb25cc81bc0598573a3405417851b
KAVYA LEKHA <KAVYA LEKHA@HP>
2019-05-10 21:14:38 +0530

    selinium implementation

---
+++ src/pack1/Google.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+	
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://www.google.com");
+
+	}
+
+}
---
+++ src/pack1/gmail.java
@@ -0,0 +1,17 @@
+package pack1;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class gmail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://www.gmail.com");
+		
+		
+	
+		
+	}
+
+}
